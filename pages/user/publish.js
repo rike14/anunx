a4f194848280d22c7aacd0eff175804388b4ae60
Henrique Kronhardt <henriquemk00@gmail.com>
2023-01-23 16:04:34 -0300

    add styles e form on the publish

--- pages/user/publish.js
+++ pages/user/publish.js
@@ -4,6 +4,7 @@ import {
     Box,
     TextField,
     Select,
+    Button,
 }   from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 
@@ -13,6 +14,9 @@ import TemplateDefault from '../../src/templates/Default'
 const useStyles = makeStyles((theme) => ({
     container: {
         padding: theme.spacing(8, 0, 6),
+    },
+    boxContainer: {
+        padding: theme.spacing(3),
     },
     box: {
         backgroundColor: theme.palette.background.white,
@@ -33,7 +37,7 @@ const Publish = () => {
                    The more detailed, better!
                 </Typography>
             </Container>
-            <Container maxWidth="md">
+            <Container maxWidth="md" className={classes.boxContainer}>
                 <Box className={classes.box}>
                     <Typography variant="h6" component="h6" align='left' color='primary'>
                         Ad Title
@@ -61,6 +65,66 @@ const Publish = () => {
                         <option value={30}>Thirty</option>
 
                         </Select>
+                </Box>
+            </Container>
+            <Container maxWidth="md" className={classes.boxContainer}>
+                <Box className={classes.box}>
+                    <Typography variant="h6" component="h6" align='left' color='textPrimary'>
+                        Images
+                    </Typography>
+                    <Typography variant="div" component="body2" align='left' color='textPrimary'>
+                        The first image will be the cover
+                    </Typography>
+                </Box>
+            </Container>
+            <Container maxWidth="md" className={classes.boxContainer}>
+                <Box className={classes.box}>
+                    <Typography variant="h6" component="h6" align='left' color='textPrimary'>
+                        Description
+                    </Typography>
+                    <Typography variant="div" component="body2" align='left' color='textPrimary'>
+                        Describe your product in detail
+                    </Typography>
+                    <TextField 
+                        rows={6}
+                        multiline
+                        fullWidth
+                        variant='outlined'
+                    />
+                </Box>
+            </Container>
+            <Container maxWidth="md" className={classes.boxContainer}>
+                <Box className={classes.box}>
+                    <Typography variant="h6" component="h6" align='left' color='textPrimary' gutterBottom>
+                        Contacts
+                    </Typography>
+                    <TextField 
+                        label='Name'
+                        fullWidth
+                        size='small'
+                        variant='outlined'
+                    />
+                    <br /><br />
+                    <TextField 
+                        label='E-mail'
+                        fullWidth
+                        size='small'
+                        variant='outlined'
+                    />
+                    <br /><br />
+                    <TextField 
+                        label='Phone'
+                        fullWidth
+                        size='small'
+                        variant='outlined'
+                    />
+                </Box>
+            </Container>
+            <Container maxWidth="md" className={classes.boxContainer}>
+                <Box textAlign='right'>
+                   <Button variant='contained' color='primary'>
+                        Publish Ad
+                   </Button>
                 </Box>
             </Container>
         </TemplateDefault>
