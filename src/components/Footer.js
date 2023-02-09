20db8ce6e3a330b3b2be42a30c8fa20b4edffdbb
Henrique Kronhardt <henriquemk00@gmail.com>
2023-02-08 23:59:05 -0300

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,68 @@
+import { Box, Container, Grid, makeStyles, Typography } from "@material-ui/core";
+import Link from "next/link";
+
+const useStyles = makeStyles((theme) => ({
+    footer: {
+        borderTop: `1px solid ${theme.palette.divider}`,
+        paddingTop: theme.spacing(3),
+        paddingBottom: theme.spacing(3),
+        marginTop: theme.spacing(8),
+        [theme.breakpoints.up('sm')]: {
+            paddingTop: theme.spacing(6),
+            paddingBottom: theme.spacing(6),
+        },
+    },
+    footerLink: {
+        textDecoration: 'none',
+    }
+}));
+
+const Footer = () => {
+    const classes = useStyles();
+
+    return (
+        <Container maxWidth="lg" component='footer' className={classes.footer}> 
+            <Grid container spacing={3}>
+                <Grid item sm={6} md={3}>
+                    <Box textAlign='center'>
+                        <Link href='#' passHref className={classes.footerLink}>
+                            <Typography color="textSecondary" variant="subtitle1">
+                                Help and Contact
+                            </Typography>
+                        </Link> 
+                    </Box>
+                </Grid>
+                <Grid item sm={6} md={3}>
+                    <Box textAlign='center'>
+                        <Link href='#' passHref className={classes.footerLink}>
+                            <Typography color="textSecondary" variant="subtitle1">
+                                Safety tips
+                            </Typography>
+                        </Link> 
+                    </Box>
+                </Grid>
+                <Grid item sm={6} md={3}>
+                    <Box textAlign='center'>
+                        <Link href='#' passHref className={classes.footerLink}>
+                            <Typography color="textSecondary" variant="subtitle1">
+                                Advertise and Sell
+                            </Typography>
+                        </Link> 
+                    </Box>
+                </Grid>
+                <Grid item sm={6} md={3}>
+                    <Box textAlign='center'>
+                        <Link href='#' passHref className={classes.footerLink}>
+                            <Typography color="textSecondary" variant="subtitle1">
+                                Professional plan
+                            </Typography>
+                        </Link> 
+                    </Box>
+                </Grid>
+            </Grid>
+        </Container>
+    
+    )
+
+}
+export default Footer;
--- src/templates/Default.js
+++ src/templates/Default.js
@@ -1,3 +1,4 @@
+import Footer from '../components/Footer'
 import Header from '../components/Header'
 
 const Default = ({children}) => {
@@ -6,7 +7,7 @@ const Default = ({children}) => {
         <>
         <Header />
         {children}
-        <footer>FOOTER</footer>
+        <Footer />
         </>
     )
 }
